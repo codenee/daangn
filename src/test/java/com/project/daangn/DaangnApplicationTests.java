a952f3b42f5077ec55940b77a42ed836e4dc5c53
codenee <tanedoran@gmail.com>
2023-12-10 19:28:39 +0900

    [#1]프로젝트 생성

---
+++ src/test/java/com/project/daangn/DaangnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.daangn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
